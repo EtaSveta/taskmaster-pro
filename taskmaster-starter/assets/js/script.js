abd49f48fb57512afccdd84d1131b6e78d00f865
Svetlana Nesterova <sveta59n@yahoo.com>
2022-09-02 14:40:40 -0400

    UX enhancements

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -62,8 +62,7 @@ var auditTask = function(taskEl){
   }
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
-  }
-
+  }  
 };
 
 $(".card .list-group").sortable({
@@ -72,16 +71,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag")
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event)  {
-  console.log("out", event.target);
+  $(event.target).removeClass("dropover-active");
  },
   update: function(event) {
     var tempArr = [];
@@ -139,10 +140,10 @@ $("#trash").droppable({
     ui.draggable.remove();
   },
   over: function(event, ui){
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active")
   }
 })
 
@@ -256,7 +257,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -290,3 +291,8 @@ $("#remove-tasks").on("click", function() {
 loadTasks();
 
 
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  }) 
+}, 1800000);
